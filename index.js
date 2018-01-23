804f513c9f346a030fd9d9bddd3d18872ace1e33
Rick Battipede <rbattipede@gmail.com>
2018-01-23 20:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {sandwich: "bread", sushi: "crab"}
 
 function updateObjectWithKeyAndValue(object, key, value){
-return object.assign({}, object, {[key]: value})
+return Object.assign({}, object, {[key]: value})
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-return object.assign( object, {[key]: value})
+return Object.assign(object, {[key]: value})
 }
 
 
